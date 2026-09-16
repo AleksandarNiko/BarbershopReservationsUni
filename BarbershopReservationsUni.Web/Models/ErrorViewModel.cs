525e00ab1101f1fcb75f6da3bf566435ade6a3ed
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2026-09-16 22:55:34 +0300

    Add project files.

---
+++ BarbershopReservationsUni.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BarbershopReservationsUni.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
